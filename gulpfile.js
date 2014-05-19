c454349331ccbec73e3a05cfb4235d6786078230
olegsmith <olegsmith@gmail.com>
2014-05-19 15:39:48 +0400

    add browserify, lint

--- gulpfile.js
+++ gulpfile.js
@@ -15,10 +15,16 @@ var livereloadport = 35729,
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify'),
 
-
 	express = require('express'),
 	server = express(),
-	livereload = require('connect-livereload');
+	livereload = require('connect-livereload'),
+
+	browserify = require('browserify'),
+	browserify_shim = require('browserify-shim'),
+	exorcist = require('exorcist'),
+	source = require('vinyl-source-stream'),
+
+	jshint = require('gulp-jshint');
 
 server.use(express.static(path.resolve('./dist')));
 
@@ -36,8 +42,8 @@ gulp.task('styles', function () {
 			sourceComments: 'map',
 			includePaths: [
 				'./src/assets/scss/theme',
-				"./src/vendor/bootstrap-sass-official/vendor/assets/stylesheets",
-				"./src/vendor/font-awesome/scss"
+				'./src/vendor/bootstrap-sass-official/vendor/assets/stylesheets',
+				'./src/vendor/font-awesome/scss'
 			].concat(bourbon),
 			errLogToConsole: true
 		}))
@@ -45,7 +51,7 @@ gulp.task('styles', function () {
 });
 
 gulp.task('html', function() {
-	return gulp.src(["./src/**/*", "!./src/assets/**", "!./src/vendor{,/**}"])
+	return gulp.src(['./src/**/*', '!./src/assets/**', '!./src/vendor{,/**}'])
 		.pipe(gulp.dest('./dist'));
 });
 
@@ -57,13 +63,48 @@ gulp.task('bootstrap:js', function() {
 });
 
 gulp.task('bootstrap:font', function() {
-	return gulp.src(["./src/vendor/bootstrap-sass-official/vendor/assets/fonts/bootstrap/**"])
+	return gulp.src(['./src/vendor/bootstrap-sass-official/vendor/assets/fonts/bootstrap/**'])
 		.pipe(gulp.dest('./dist/assets/fonts'));
 });
 
 gulp.task('fontawesome:font', function() {
-	return gulp.src([ "./src/vendor/font-awesome/fonts/**"])
+	return gulp.src([ './src/vendor/font-awesome/fonts/**'])
 		.pipe(gulp.dest('./dist/assets/fonts'));
+});
+
+gulp.task('jquery:js', function() {
+	return gulp.src([ './src/vendor/jquery/dist/**'])
+		.pipe(gulp.dest('./dist/assets/js'));
+});
+
+gulp.task('js:lint', function() {
+	return gulp.src("./src/assets/js/**/*.js")
+		.pipe(jshint('.jshintrc'))
+		.pipe(jshint.reporter());
+});
+
+gulp.task('js:bundle', function() {
+	var bundler = browserify()
+		.add('./src/assets/js/bm.js');
+
+	return bundler.bundle({debug: true})
+		.on('error', function (err) {
+			console.log(err.toString());
+			this.emit("end");
+		})
+		.pipe(exorcist('./dist/assets/js/bm.js.map'))
+		.pipe(source("bm.js"))
+		.pipe(gulp.dest('./dist/assets/js'));
+});
+
+gulp.task('js:min', function() {
+  gulp.src('./dist/assets/js/bm.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/assets/js'))
+});
+
+gulp.task('js', function(done) {
+	runSequence('js:lint', 'js:bundle', 'js:min', done);
 });
 
 gulp.task('serve', function() {
@@ -71,18 +112,11 @@ gulp.task('serve', function() {
 });
 
 gulp.task('watch', function () {
-	// gulp.watch('src/assets/js/**', ['lint','app', 'core']);
-	// gulp.watch('src/assets/jsext/**', ['extscripts']);
-	// gulp.watch('src/assets/scss/**', ['styles']);
-	// gulp.watch('src/assets/icons/**', ['iconfont']);
-	// gulp.watch('src/assets/img/**', ['img']);
-	// gulp.watch(['src/**/*.*',  "!src/**/*.hbs", '!src/assets/**'], ['html']);
-	// gulp.watch(['src/**/*.hbs', '!src/assets/**'], ['yassemble']);
-	// gulp.watch(['src/assets/yassemble/**/*.hbs'], ['yassemble']);
+	gulp.watch('src/assets/js/**', ['js']);
 });
 
 gulp.task('build', function(done) {
-	runSequence('bower', ['styles', 'html', 'bootstrap:js', 'bootstrap:font', 'fontawesome:font'], done);
+	runSequence('bower', ['styles', 'html', 'bootstrap:js', 'bootstrap:font', 'fontawesome:font', 'jquery:js', 'js'], done);
 });
 
 gulp.task('run', function(done) {
